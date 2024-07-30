15c6f17a8c4799ed98988855f8c8547fd530d69c
Akhil Sharma <akhilsharma@Akhils-MacBook-Air-3.local>
2024-07-30 19:36:39 +0530

    dashboard updated

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import Navbar from './Navbar';
 function Dashboard() {
   return (
     <div className="dashboard">
+      <div>
+        <Navbar/>
+      </div>
       <h1 style={{ textAlign: "center" }}>Dashboard</h1>
       <p>Welcome to the AI-Driven Skill Exchange Network!</p>
     </div>
